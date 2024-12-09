f096632d116fc445d0e04bd829f53a081788faad
VeerinradaP <wrbv7884@leeds.ac.uk>
2024-12-09 15:27:25 +0000

    Update main.cpp

--- Water Quality Monitoring/main.cpp
+++ Water Quality Monitoring/main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.hpp"
 #include "datamanager.hpp"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator trn;
+    if (trn.load(QLocale::system(), "watertool", "_", ":/i18n")) {
+        a.installTranslator(&trn);
+    }
 
     DataManager dataManager;
     dataManager.loadData("data.csv");
